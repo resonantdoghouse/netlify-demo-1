f4d99511652a158012ecfaed895d3872497a0643
Jim <jim@advancebass.com>
2023-08-17 17:38:44 -0700

    add devjokes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 // import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://developerjokes.herokuapp.com/jokes').then((response) => {
+      setJokes(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello, Netlify 👋</h1>
+      <ul>
+        {jokes.map((joke) => (
+          <li>{joke.question}</li>
+        ))}
+      </ul>
     </div>
   );
 }
